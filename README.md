c5db091b2c5b0fe504bbb058038f111d3c70de9d
Sudharshan Govindan <sudharshan.govindan@in.ibm.com>
2020-06-25 15:06:56 +0530

    changes

--- README.md
+++ README.md
@@ -1,25 +1,26 @@
-# Deploy simple Quiz application and take the quiz
+# Deploy simple application and submit the Questionnaire
 
-[Deploy to IBM Cloud](https://cloud.ibm.com/devops/setup/deploy?repository=https://github.com/sudharshan-govindan/skill-app.git)
+[Deploy to IBM Cloud](https://cloud.ibm.com/devops/setup/deploy?repository=https://github.com/ragdeshp/skill-app.git)
 
 1. Press the above `Deploy to IBM Cloud` button and then...
 
+   * Click `Delivery Pipeline` icon.
    * Click `Create+` to create an IBM Cloud API Key.
    * Select your region, organization, and space (or use the defaults).
-   * Click `Deploy`.
+   * Click `Create` on top right corner to create a ToolChain.
 
-2. In Toolchains, click on `Delivery Pipeline` to watch while the app is deployed.
+2. In Toolchains, click on `Delivery Pipeline` to watch while the app is deployed. 
 
    ![toolchain-pipeline](doc/images/tool.png)
-
+   
 3. Click on `View Console` in the Deploy stage
-
+   
    ![toolchain-pipeline](doc/images/1.png)
-
+   
 4. Once deployed, the app can be viewed by clicking `Visit App URL`
 
    ![toolchain-pipeline](doc/images/2.png)
 
-5. Attempt the Quiz in the deployed app and submit the response to view your score!
+5. Attempt the Questions in the deployed app and submit the questionnaire!
 
-   ![toolchain-pipeline](doc/images/3.png)
+   ![toolchain-pipeline](doc/images/4.png)
--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
-var Cloudant = require('cloudant');
+//var cloudant = require('cloudant');
 var express = require('express');
 var bodyParser = require('body-parser');
 var stringSimilarity = require('string-similarity');
-//let secureEnv = require('secure-env');
-// = secureEnv({secret:'mySecretPassword'});
+let secureEnv = require('secure-env');
+//global.env = secureEnv({secret:'mySecretPassword'});
 
 var app = express();
 
@@ -16,48 +16,32 @@ app.get('/',function(req,res){
   res.sendFile("views/index.html", {"root": __dirname});
 });
 
-var cloudant = Cloudant({account:process.env.cloudantusername, password:process.env.cloudantpassword});
+var Cloudant = require('@cloudant/cloudant');
+var cloudant = Cloudant({url: "https://fcec4451-f26d-4ee1-aada-480743f01158-bluemix:d7222383947d82f7abf6bd69438f1bccb8d3ce76b23233b64777d98c077685f7@fcec4451-f26d-4ee1-aada-480743f01158-bluemix.cloudantnosqldb.appdomain.cloud"});
+var dbname = 'mydb';
+
+
+//var cloudant = cloudant({account:global.env.cloudantusername, password:global.env.cloudantpassword});
 
 var responsequiz1;
 var responsequiz2;
-var responsequiz3;
-var responsequiz4;
-var responsequiz5;
+
 var time;
 var url;
 var counter=0;
 
-quizdb = cloudant.db.use(process.env.dbname);
+quizdb = cloudant.db.use(dbname);
 
 
 app.post('/quizsubmission', function(req,res){
-    if(stringSimilarity.compareTwoStrings(req.body.quiz_1,'b')){
-       counter++;
-       }
-    if(stringSimilarity.compareTwoStrings(req.body.quiz_2,'d')){
-        counter++;
-    }
-    if(stringSimilarity.compareTwoStrings(req.body.quiz_3,'c')){
-       counter++;
-       }
-    if(stringSimilarity.compareTwoStrings(req.body.quiz_4,'c')){
-        counter++;
-    }
-    if(stringSimilarity.compareTwoStrings(req.body.quiz_5,'b')){
-       counter++;
-       }
-    var quizscore = counter;
+
     var doc={
-        _id:req.headers.host + req.url,
-        score:quizscore,
+        _id:req.body.cloudemail,
         responsequiz1: req.body.quiz_1,
         responsequiz2: req.body.quiz_2,
         responsequiz3: req.body.quiz_3,
-        responsequiz4: req.body.quiz_4,
-        responsequiz5: req.body.quiz_5,
         time: new Date().toISOString(),
-        email: req.body.cloudemail,
-        interested: req.body.interested
+        email: req.body.cloudemail
         };
 
 quizdb.insert(doc,function(err,body,header){
@@ -68,8 +52,8 @@ quizdb.insert(doc,function(err,body,header){
     }
     else{
         res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<center><h2>Thank you for taking the quiz!</h2><center>');
-    res.write('<center><h3>You have scored '+quizscore+'!</h3><center>');
+    res.write('<center><h2>Thank you for answering the questions !</h2><center>');
+    //res.write('<center><h3>You have scored '+quizscore+'!</h3><center>');
     res.end();
     }
 });
